c14104bf168eaf52988f081a63221ec9fc7bd777
Jeffrey <Jparkerpowell@gmail.com>
2024-11-02 10:46:07 +0100

    working with CBS data, except for forecast plus series plot

--- code/02_forecast_CBS_GDP_ets_loop.R
+++ code/02_forecast_CBS_GDP_ets_loop.R
@@ -35,17 +35,35 @@ library(lmtest)
 ##########################
 
 # load data
-dt1 <- read.csv("data/HandelDiensten_raw.csv", sep = ",")
-colnames(dt1)
+dt1 <- read.csv("data/cbs_basic_macro_NOT_SEASONCORRECTED_qt.csv", sep = ",")
 
-colName <- "X45.Autohandel.en..reparatie"
+# not appropriate for negative numbers
+drops <- c("change_supply_season")
+dt1 <- dt1[ , !(names(dt1) %in% drops)]
+
+
+colnames(dt1)
+dim(dt1)
+
+allColumns <- colnames(dt1)
+
+# remove Perioden
+allColumns_1 <- allColumns[c(-1)]
+
+##########################
+##########################
+##########################
+for(colName in allColumns_1){ 
+
+print(colName)
+
+#colName <- "Detailhandel.excl..auto.s..tankstations"
 
 # connects all the data
 Key1 <- paste(Sys.Date(), "_", colName, sep="")
 
-series1 <- ts(dt1[colName], frequency = 12, start=c(2000,1))
+series1 <- ts(dt1[colName], frequency = 4, start=c(1995,1))
 series1 <- na.omit(series1)
-
 
 #########################
 # Which model to use
@@ -121,7 +139,7 @@ modelform <- str_c(c(errortype, trendtype, seasontype), collapse = "")
 
 fit <- ets(series1, model=modelform, damped=FALSE)
 
-h1 <- 12
+h1 <- 4
 train <- head(series1, round(length(series1) - h1))
 test <- tail(series1, h1)
 
@@ -129,7 +147,7 @@ fit <- ets(train, model=modelform, damped=FALSE)
 forecasted1 <- forecast(fit, h=h1)
 
 png(filename=paste("output/figures/", Key1, "TrainTestForecast.png", sep = "_"))
-autoplot(forecasted1, include=h1+24) + autolayer(test)
+print(autoplot(forecasted1, include=h1+6) + autolayer(test) + ggtitle(colName))
 dev.off()
 
 ####################
@@ -139,7 +157,7 @@ fit <- ets(series1, model=modelform, damped=FALSE)
 forecast_oneMonth <- forecast(fit, h=1)
 
 png(filename=paste("output/figures/", Key1, "final_forecasts.png", sep = "_"))
-autoplot(tail(series1, 36)) + autolayer(forecast_oneMonth) + ggtitle(colName)
+print(autoplot(tail(series1, 9)) + autolayer(forecast_oneMonth) + ggtitle(colName))
 dev.off()
 
 ################################
@@ -158,6 +176,9 @@ data <- data.frame(
   RawData = series1
 )
 data$Key1 <- Key1
+
+colnames(data) <- c("SeriesName", "DateAnalysis", "ETSmodel", "ObservationDate", "RawData", "Key1")
+
 write.table(data, file = paste("output/forecasts/", Key1, "RawData.csv", sep="_"), sep =",",row.names = FALSE)
 
 ###
@@ -177,3 +198,84 @@ finalForecast$Key1 <- Key1
 finalForecast <- tibble::rownames_to_column(finalForecast, "Forecast_Period")  
 
 write.table(finalForecast, file = paste("output/forecasts/", Key1, "final_forecasts.csv", sep="_"), sep =",",row.names = FALSE)
+
+} 
+############# END LOOP ##############
+############# END LOOP ##############
+############# END LOOP ##############
+
+##############################
+# Possible analyses
+##############################
+
+###
+# Combine all forecasts
+###
+
+files <- list.files("output/forecasts" , pattern = "final_forecasts.*\\.csv$", full.names = TRUE)
+initial_df <- read.csv(files[1])
+
+# Merge data frames using a loop
+for (fl in files[2:length(files)]) {
+  file_df <- read.csv(fl)
+  initial_df <- rbind(initial_df, file_df)
+}
+# View the merged data frame
+print(initial_df)
+write.table(initial_df, file = "output/analyses/combined_final_forecasts.csv", sep =",",row.names = FALSE)
+
+###
+# Combine point forecast + entire series 
+###
+
+files_Raw <- list.files("output/forecasts" , pattern = "RawData.*\\.csv$", full.names = TRUE)
+files_FinalForecasts <- list.files("output/forecasts" , pattern = "final_forecasts.*\\.csv$", full.names = TRUE)
+
+###################################
+###################################
+###################################
+index <- 0
+for(fl in files_Raw) {
+
+index <- index + 1
+
+print(index)
+
+# get raw data
+firstfile <- files_Raw[index]
+initial_df_raw <- read.csv(firstfile)
+tmp_raw <- initial_df_raw[,c(4,5)]
+
+series_name <- initial_df_raw[,c(1)][1]
+
+# get final forecasts
+firstfile <- files_FinalForecasts[index]
+initial_df_finalfore <- read.csv(firstfile)
+tmp_forecast <- initial_df_finalfore[,c(1,2)]
+
+# copy column names
+colnames(tmp_forecast) <- colnames(tmp_raw)
+combined <- rbind(tmp_raw, tmp_forecast)
+
+# change to proper date
+combined$ObservationDate <- as.Date(paste(1, combined$ObservationDate), '%d %B %Y')
+
+# colors for different parts of line
+combined$mycolors <- c(rep('hist', length(combined[,1])-2), rep(c('forecast'),2))
+
+png(filename=paste("output/figures/", series_name, "series_and_forecast.png", sep = "_"))
+ggplot(combined, aes(x =  ObservationDate, y = 'Raw', colour = mycolors, group = 1)) +
+geom_line()
+dev.off()
+
+###
+# Combine point forecast + entire series (using data above)
+###
+combined$seriesDifferenced <- combined['RawData'] - lag(combined['RawData'], 4)
+combined$seriesDifferenced <- unlist(combined$seriesDifferenced)
+
+png(filename=paste("output/figures/", series_name, "differenced_forecasts.png", sep = "_"))
+plot(ts(combined[,c(2,4)], frequency = 4, start=c(1995,1)), main=series_name)
+dev.off()
+
+}
