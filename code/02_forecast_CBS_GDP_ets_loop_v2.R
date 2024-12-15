456628ee6d6f97b3841a563810f4e151acf44b61
Jeffrey <Jparkerpowell@gmail.com>
2024-12-15 12:07:38 +0100

    Generalized code a bit, seems to be working

---
+++ code/02_forecast_CBS_GDP_ets_loop_v2.R
@@ -0,0 +1,305 @@
+library(tidyverse)
+library(xts)
+library(zoo)
+library(svglite)
+library(TSstudio)
+library(zoo)
+library(dlm)
+library(forecast)
+library(expsmooth)
+library(ggplot2)
+#library(ggfortify)
+library(changepoint)
+library(KFAS)
+library(httpgd)
+library(funtimes)
+library(seastests)
+library(car)
+library(lmtest)
+
+# Time series plots
+# https://cran.rstudio.com/web/packages/ggfortify/vignettes/plot_ts.html
+
+
+# Model consists of three letters following Hyndman (2008) and here: https://search.r-project.org/CRAN/refmans/forecast/html/ets.html
+
+# First letter is the error type:     A, M or Z
+# Second letter is the trend type:    N, A, M, Z
+# Third letter is the season type:    N, A, M, Z
+
+# Some of the models have names:
+#     ANN is simple exponential smoothing with additive errors.
+#     MAM is multiplicative Holt-Winters with multiplicative errors.
+
+
+appropriateModels <- c("ANNX", "ANAX", "ANMX",
+                       "AANX", "AAAX", "AAMX",
+                       "AAND", "AAAD", "ANMD",
+                       "AMNX", "AMAX", "AMMX",
+                       "AMND", "AMAD", "AMMD",
+                       "MNNX", "MNAX", "MNMX",
+                       "MANX", "MAAX", "MAMX",
+                       "MAND", "MAAD", "MNMD",
+                       "MMNX", "MMAX", "MMMX",
+                       "MMND", "MMAD", "MMMD"
+                       )
+
+##########################
+
+# load data
+# dt1 <- read.csv("data/HandelDiensten_raw1_2024_11_01.csv", sep = ",")
+dt1 <- read.csv("data/a0_combinedQuarterly_extended_ETS.csv", sep=",", stringsAsFactors = FALSE)
+
+rownames(dt1) <- dt1$X
+
+colnames(dt1)
+dim(dt1)
+allColumns <- colnames(dt1)
+
+# remove perioden column
+data_columns <- allColumns[c(-1)]
+
+##########################
+# ending date
+##########################
+start_date <- "1995-01-01"
+mystart = c(1995,1)
+end_date <- "2024-04-01"
+
+dt1 %>% filter(rownames(dt1) >= start_date &  rownames(dt1) <= end_date) -> dt1
+
+##########################
+##########################
+##########################
+
+for(colName in data_columns){ 
+
+  print(colName)
+
+  # test case
+  #colName <- "gdp_total"
+
+  # connects all the data
+  Key1 <- paste(Sys.Date(), "_", colName, sep="")
+
+  series1 <- ts(dt1[colName], frequency = 4, start=mystart)
+  series1 <- na.omit(series1)
+
+  #########################
+  # Which model to use
+  #########################
+
+  bestMod <- function(appropriateModels, data, lowest_aic = Inf){
+
+    ###########################
+    horizon1 <- 2
+    finalforecastHorizon <- 2
+    ###########################
+
+      for (mod in appropriateModels){
+
+      tryCatch({
+
+          ##############################
+          #print(mod)  
+
+          error1 <- str_sub(mod, 1, 1)
+          trend2 <- str_sub(mod, 2, 2)
+          season3 <- str_sub(mod, 3, 3)
+          damp4 <- str_sub(mod, 4, 4)
+          ##############################
+
+      if(damp4 == "X"){ #damped is FALSE
+              model <- ets(data, model = mod, damped = FALSE)
+              out1 <- list(mod, model$aic, damped = FALSE)
+
+              } else { #damped is TRUE
+                  model <- ets(data, model = mod, damped = TRUE)
+
+                  out1 <- list(mod, model$aic, damped = TRUE)
+                  #print(out1[[2]])
+              }
+      }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})    
+
+      new_aic <- out1[[2]]
+
+      if (new_aic < lowest_aic){
+          lowest_aic <- out1[[2]]
+          best_model <- out1
+      }
+      }
+
+      bm_df <- t(as.data.frame(matrix(unlist(best_model)),nrow=1,ncol=3))  
+
+      colnames(bm_df) <- c("Model", "AIC", "Damped")
+      rownames(bm_df) <- c("model")
+
+      return(bm_df)
+  }
+
+  #########################
+  # Which model to use
+  #########################
+
+  choosenModel <- bestMod(appropriateModels, series1)
+  modelform <- choosenModel[1,1]
+  dampform  <- choosenModel[1,3]
+
+  if(dampform == "FALSE"){
+    fit <- ets(series1, model=modelform, damped=FALSE)
+  } else {
+    fit <- ets(series1, model=modelform, damped=TRUE)
+  }
+  
+  train <- head(series1, round(length(series1) - horizon1))
+  test <- tail(series1, horizon1)
+
+   if(dampform == "FALSE"){
+    fit <- ets(train, model=modelform, damped=FALSE)
+  } else {
+    fit <- ets(train, model=modelform, damped=TRUE)
+  }
+  forecasted1 <- forecast(fit, h=horizon1)
+
+  png(filename=paste("output/figures/", Key1, "TrainTestForecast.png", sep = "_"))
+  print(autoplot(forecasted1, include=horizon1+2) + autolayer(test) + ggtitle(colName))
+  dev.off()
+
+  ####################
+  # final forecast
+  ####################
+
+  if(dampform == "FALSE"){
+    fit <- ets(series1, model=modelform, damped=FALSE)
+  } else {
+    fit <- ets(series1, model=modelform, damped=TRUE)
+  }
+
+  forecast_oneMonth <- forecast(fit, h=finalforecastHorizon)
+
+  png(filename=paste("output/figures/", Key1, "final_forecasts.png", sep = "_"))
+  print(autoplot(tail(series1, 2)) + autolayer(forecast_oneMonth) + ggtitle(colName))
+  dev.off()
+
+  ################################
+  # Saving
+  ################################
+
+  ###
+  # Raw Data
+  ###
+
+  data <- data.frame(
+    SeriesName   = colName, 
+    DateAnalysis = Sys.Date(), 
+    ETSmodel = modelform,
+    ObservationDate = as.yearqtr(time(series1)),
+    RawData = series1
+  )
+  data$Key1 <- Key1
+
+  colnames(data) <- c("SeriesName", "DateAnalysis", "ETSmodel", "ObservationDate", "RawData", "Key1")
+
+  write.table(data, file = paste("output/forecasts/", Key1, "RawData.csv", sep="_"), sep =",",row.names = FALSE)
+
+  ###
+  # TrainTestForecast
+  ###
+  forecast_tibble <- as.data.frame(forecasted1)
+  forecast_tibble$Key1 <- Key1 
+
+  write.table(forecast_tibble, file = paste("output/forecasts/", Key1, "TrainTestForecast.csv", sep="_"), sep =",",row.names = FALSE)
+
+  ###
+  # finalForecast
+  ###
+  forecast_oneMonth <- forecast(fit, h=finalforecastHorizon)
+  finalForecast <- as.data.frame(forecast_oneMonth, row.names = NULL)
+  finalForecast$Key1 <- Key1
+  finalForecast <- tibble::rownames_to_column(finalForecast, "Forecast_Period")  
+
+  write.table(finalForecast, file = paste("output/forecasts/", Key1, "final_forecasts.csv", sep="_"), sep =",",row.names = FALSE)
+
+} 
+
+
+
+
+
+############# END LOOP ##############
+############# END LOOP ##############
+############# END LOOP ##############
+
+##############################
+# Possible analyses
+##############################
+
+###
+# Combine all forecasts
+###
+
+files <- list.files("output/forecasts" , pattern = "final_forecasts.*\\.csv$", full.names = TRUE)
+initial_df <- read.csv(files[1])
+
+# Merge data frames using a loop
+for (fl in files[2:length(files)]) {
+  file_df <- read.csv(fl)
+  initial_df <- rbind(initial_df, file_df)
+}
+# View the merged data frame
+print(initial_df)
+write.table(initial_df, file = "output/analyses/combined_final_forecasts.csv", sep =",",row.names = FALSE)
+
+###
+# Combine point forecast + entire series 
+###
+
+files_Raw <- list.files("output/forecasts" , pattern = "RawData.*\\.csv$", full.names = TRUE)
+files_FinalForecasts <- list.files("output/forecasts" , pattern = "final_forecasts.*\\.csv$", full.names = TRUE)
+
+###################################
+###################################
+###################################
+index <- 0
+for(fl in files_Raw) {
+
+index <- index + 1
+
+print(index)
+
+# get raw data
+firstfile <- files_Raw[index]
+initial_df_raw <- read.csv(firstfile)
+tmp_raw <- initial_df_raw[,c(4,5)]
+
+series_name <- initial_df_raw[,c(1)][1]
+
+# get final forecasts
+firstfile <- files_FinalForecasts[index]
+initial_df_finalfore <- read.csv(firstfile)
+tmp_forecast <- initial_df_finalfore[,c(1,2)]
+
+# copy column names
+colnames(tmp_forecast) <- colnames(tmp_raw)
+combined <- rbind(tmp_raw, tmp_forecast)
+
+# change to proper date
+combined$ObservationDate <- as.Date(paste(1, combined$ObservationDate), '%d %B %Y')
+
+# colors for different parts of line
+combined$mycolors <- c(rep('hist', length(combined[,1])-2), rep(c('forecast'),2))
+
+ggplot(combined, aes(x =  ObservationDate, y = 'Raw', colour = mycolors, group = 1)) +
+geom_line()
+
+###
+# Combine point forecast + entire series (using data above)
+###
+combined$seriesDifferenced <- combined['RawData'] - lag(combined['RawData'], 12)
+combined$seriesDifferenced <- unlist(combined$seriesDifferenced)
+
+png(filename=paste("output/figures/", series_name, "differenced_forecasts.png", sep = "_"))
+plot(ts(combined[,c(2,4)], frequency = 12, start=c(2000,1)), main=series_name)
+dev.off()
+
+}
